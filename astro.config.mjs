5e25b90d5166280fb6cd1c3f90a58be7ef56b283
nico <nico@MacBook-Air.local>
2024-03-04 12:18:19 -0500

    Fixed spanish section

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,7 +8,7 @@ export default defineConfig({
   integrations: [tailwind(), icon()],
 
   i18n: {
-    defaultLocale: "en",
+    defaultLocale: "es",
     locales: ["en", "es"],
     routing: {
       prefixDefaultLocale: true,
