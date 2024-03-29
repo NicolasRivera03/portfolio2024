e828be2af129791f92fdb98a883b4ad3372e9347
Nicolás Rivera <riveramoranicolas@gmail.com>
2024-03-29 14:48:30 -0500

    Added sidebar

--- astro.config.mjs
+++ astro.config.mjs
@@ -11,7 +11,7 @@ export default defineConfig({
     defaultLocale: "es",
     locales: ["en", "es"],
     routing: {
-      prefixDefaultLocale: true,
+      prefixDefaultLocale: false,
     },
   },
 });
